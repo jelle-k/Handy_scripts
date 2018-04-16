4262f39b1b8fff6f96dab19e51a89ae9c6f5f243
Jelle <jellep@kakebeeke.net>
2018-04-16 11:12:34 +0200

    Cleanup

---
+++ XML-XSLT/Ext_SBO/processor.py
@@ -0,0 +1,20 @@
+import lxml.etree as ET
+import time
+
+try:
+  print("Running...")
+  while True: 
+    dom = ET.parse(r"C:\Users\jkakebeeke\Documents\Handy scripts\XML-XSLT\Ext_SBO\Input.xml")
+    xslt = ET.parse(r"C:\Users\jkakebeeke\Documents\Handy scripts\XML-XSLT\Ext_SBO\Ext_GRPO.xslt")
+    transform = ET.XSLT(xslt)
+    newdom = transform(dom)
+    # print(ET.tostring(newdom, pretty_print=True))
+    with open('out.xml', 'w') as f:
+      print(newdom, file=f)
+    f.close()
+    time.sleep(5)
+except KeyboardInterrupt:
+  print("Ended")
+  
+
+
